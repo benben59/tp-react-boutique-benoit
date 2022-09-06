f5b23c2c5ecbe01faa582ce4b227b2a92d8f8037
benoit <marliere.benoit59@gmail.com>
2022-09-06 09:20:25 +0200

    E1

---
+++ src/Components/CategoryCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const CategoryCard = () => {
+  return (
+    <div>
+      <>
+        <div className="container">
+          <div class="card">
+            <img
+              src="https://www.pika.fr/sites/default/files/images/livres/couv/-T.jpg"
+              alt="Category"
+            />
+          <h4>
+            <b>Category title</b>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            iste numquam tenetur quas.
+          </p>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default CategoryCard;
---
+++ src/Pages/Homescreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CategoryCard from '../Components/CategoryCard';
+
+const Homescreen = () => {
+    return (
+        <div>
+            <h1>La boutique de Benoît Marlière</h1>
+            <CategoryCard />
+        </div>
+    );
+};
+
+export default Homescreen;

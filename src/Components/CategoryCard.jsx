03d1e09bed1d1c9f1f92a9daf75202a08b39a567
benoit <marliere.benoit59@gmail.com>
2022-09-06 09:27:40 +0200

    E3 + 4

--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function CategoryCard(props) {
+function CategoryCard({title, description, image}) {
   return (
-    <div className='col-6'>
-  <div className="card" style={{width: '18rem'}}>
-    <img src={props.img} class="card-img-top" alt={props.user}/>
-    <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-      <p className="card-text">{props.text}</p>
+  <>
+      <div className="col-6">
+      <div className="card" style={{width: '18rem'}}>
+        <img className="card-img-top" src={image} alt="Card" />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <button href="#" className="btn btn-primary">Go somewhere</button>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
+  </>
   );
 }
 
--- src/Pages/Homescreen.jsx
+++ src/Pages/Homescreen.jsx
@@ -2,20 +2,46 @@ import React from "react";
 import CategoryCard from "../Components/CategoryCard";
 
 const Homescreen = () => {
+  const cardCategories = [
+    {
+      id: "1",
+      title: "Homme",
+      description: "La mode pour les hommes",
+      image: "../../assets/img/9782811618124-T.jpg",
+    },
+    {
+      id: "2",
+      title: "Femme",
+      description: "La mode pour les femmes",
+      image: "./assets/img/9782811618124-T.jpg",
+    },
+    {
+      id: "3",
+      title: "Enfant",
+      description: "La mode pour eux",
+      image: "./assets/img/9782811618124-T.jpg",
+    },
+    {
+      id: "4",
+      title: `Baby`,
+      description: "La mode pour eux aussi",
+      image: "./assets/img/9782811618124-T.jpg",
+    },
+  ];
+
   return (
     <div>
+      <h1>La boutique de Benoît Marlière</h1>
       <div className="row">
-        <h1>La boutique de Benoît Marlière</h1>
-        <CategoryCard
-          img="9782811618124-T.jpg"
-          title="Homme"
-          text="La mode pour les hommes"
-        />
-        <CategoryCard
-          img="9782811618124-T.jpg"
-          title="Femme"
-          text="La mode pour les femmes"
-        />
+        {cardCategories.map((card) => (
+          <CategoryCard
+            card={card}
+            title={card.title}
+            description={card.description}
+            image={card.image}
+            key={card.id}
+          />
+        ))}
       </div>
     </div>
   );
